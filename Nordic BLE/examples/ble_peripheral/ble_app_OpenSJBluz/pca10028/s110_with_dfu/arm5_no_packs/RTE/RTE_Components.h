992d6b31daa608df9f1eb7b1c09264c4f346d40f
dhruvkakadiya <dhruv.kakadiya@gmail.com>
2016-04-24 19:44:33 -0700

    Added separate application project for OpenSJBluz

---
+++ Nordic BLE/examples/ble_peripheral/ble_app_OpenSJBluz/pca10028/s110_with_dfu/arm5_no_packs/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_OpenSJBluz' 
+ * Target:  'nRF51822_xxAC' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
