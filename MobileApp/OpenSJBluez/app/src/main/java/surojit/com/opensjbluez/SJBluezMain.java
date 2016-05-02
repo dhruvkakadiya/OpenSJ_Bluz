d585bb1339436c7b2bf8c04571edd37745761b27
dhruvkakadiya <dhruv.kakadiya@gmail.com>
2016-05-02 01:16:04 -0700

    Added Settings Menu

--- MobileApp/OpenSJBluez/app/src/main/java/surojit/com/opensjbluez/SJBluezMain.java
+++ MobileApp/OpenSJBluez/app/src/main/java/surojit/com/opensjbluez/SJBluezMain.java
@@ -2,6 +2,9 @@ package surojit.com.opensjbluez;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class SJBluezMain extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class SJBluezMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sjbluez_main);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+//        if(item.getItemId() == )
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater lMenuInflater = getMenuInflater();
+        lMenuInflater.inflate(R.menu.main_menu,menu);
+
+        return super.onCreateOptionsMenu(menu);
+
     }
 }
