989a5e7e806cb11af773a02f65154a5052cdf8c6
dhruvkakadiya <dhruv.kakadiya@gmail.com>
2016-05-02 00:45:09 -0700

    Initial Commit Added blank activity, started android app

---
+++ MobileApp/OpenSJBluez/app/src/main/java/surojit/com/opensjbluez/SJBluezMain.java
@@ -0,0 +1,13 @@
+package surojit.com.opensjbluez;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SJBluezMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sjbluez_main);
+    }
+}
